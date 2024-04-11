852da8c24b0dc4264b62e4ff67b33a24f33ca509
Camilo Manuel Couselo Alonso <manuel.couseloalonso@gmail.com>
2024-04-11 17:35:47 +0200

    Update Navbar.vue

--- components/App/Navbar.vue
+++ components/App/Navbar.vue
@@ -2,13 +2,13 @@
   <div ref="headerRef" :style="styles" class="fixed top-0 w-full z-50">
     <nav class="mx-auto max-w-2xl px-2 sm:px-6 lg:px-8">
       <ul
-        class="flex justify-center items-center my-4 px-3 text-sm font-medium text-gray-800 rounded-full shadow-lg bg-white/90 shadow-gray-800/5 ring-1 backdrop-blur dark:bg-gray-800/90 dark:text-gray-200 dark:ring-white/20 ring-gray-900/5"
+        class="flex justify-between items-center my-4 px-3 text-sm font-medium text-gray-800 rounded-full shadow-lg bg-white/90 shadow-gray-800/5 ring-1 backdrop-blur dark:bg-gray-800/90 dark:text-gray-200 dark:ring-white/20 ring-gray-900/5"
       >
         <li v-for="item in items" :key="item.path">
           <UTooltip :text="item.name" :ui="{ popper: { strategy: 'absolute' } }">
             <ULink
               :to="item.path"
-              class="relative px-16 py-4 flex items-center justify-center transition hover:text-primary-500 dark:hover:text-primary-400"
+              class="relative px-4 py-4 flex items-center justify-center transition hover:text-primary-500 dark:hover:text-primary-400"
               active-class="text-primary-600 dark:text-primary-400"
             >
               <Icon aria-hidden="true" :name="item.icon" class="w-5 h-5 z-10" />
