86725d3338eee0d708521d217b26c1c2f57b5afe
Camilo Manuel Couselo Alonso <manuel.couseloalonso@gmail.com>
2024-04-12 20:26:11 +0200

    Update projects.vue

--- pages/projects.vue
+++ pages/projects.vue
@@ -3,14 +3,20 @@
     <AppHeader class="mb-12" title="Proyectos" :description="description" />
 
 
-    <AppProjectCard v-for="(project, id) in paginatedProjects" :key="id" :project="project" />
-
-
-    <div class="flex justify-center items-center my-8 px-3 ">
-      <UPagination v-model="currentPage" :page-count="pageCount" :total="totalProjects"
+    <AppProjectCard
+        v-for="(project, id) in projects"
+        :key="id"
+        :project="project"
+      />
+<!--
+<div class="flex justify-center items-center my-8 px-3 ">
+      <UPagination v-model="1" :page-count="5" :total="5"
         :prev-button="{ icon: 'i-heroicons-arrow-small-left-20-solid', label: 'Prev', color: 'gray' }"
         :next-button="{ icon: 'i-heroicons-arrow-small-right-20-solid', trailing: true, label: 'Next', color: 'gray' }" />
     </div>
+
+-->
+    
   </main>
 </template>
 
@@ -22,28 +28,8 @@ useSeoMeta({
   title: "Proyectos | Camilo M. Couselo Alonso",
   description,
 });
-
-const currentPage = ref(1);
-const itemsPerPage = 4;
-
-let proje = ref([]);
-let totalProjects = ref(0);
-let pageCount = ref(0);
-
-
-const { data, data: projects } = useLazyFetch("https://api.github.com/users/cm-ca/repos", {
+const { data: projects } = useFetch("https://api.github.com/users/cm-ca/repos",{
   method: 'GET',
 
-});
-
-
-projects.value = data.value;
-totalProjects.value = proje.value.length;
-pageCount.value = Math.ceil(totalProjects.value / itemsPerPage);
-
-const paginatedProjects = computed(() => {
-  const start = (currentPage.value - 1) * itemsPerPage;
-  const end = start + itemsPerPage;
-  return proje.value.slice(start, end);
 });
 </script>
