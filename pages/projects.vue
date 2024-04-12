2313fdfdbdc831b39b3161d53b974162becf6dba
Camilo Manuel Couselo Alonso <manuel.couseloalonso@gmail.com>
2024-04-12 20:20:53 +0200

    Update projects.vue

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,16 +1,15 @@
 <template>
   <main class="min-h-screen">
     <AppHeader class="mb-12" title="Proyectos" :description="description" />
-    <div class="space-y-4" v-if="pending">Cargando...</div>
-    <div v-else>
-      <AppProjectCard v-for="(project, id) in paginatedProjects" :key="id" :project="project" />
-    </div>
+
+
+    <AppProjectCard v-for="(project, id) in paginatedProjects" :key="id" :project="project" />
+
 
     <div class="flex justify-center items-center my-8 px-3 ">
-      <UPagination v-model="currentPage" :page-count="pageCount" :total="totalProjects" 
-      @change="fetchProjects"
-      :prev-button="{ icon: 'i-heroicons-arrow-small-left-20-solid', label: 'Prev', color: 'gray' }"
-      :next-button="{ icon: 'i-heroicons-arrow-small-right-20-solid', trailing: true, label: 'Next', color: 'gray' }"/>
+      <UPagination v-model="currentPage" :page-count="pageCount" :total="totalProjects"
+        :prev-button="{ icon: 'i-heroicons-arrow-small-left-20-solid', label: 'Prev', color: 'gray' }"
+        :next-button="{ icon: 'i-heroicons-arrow-small-right-20-solid', trailing: true, label: 'Next', color: 'gray' }" />
     </div>
   </main>
 </template>
@@ -27,28 +26,24 @@ useSeoMeta({
 const currentPage = ref(1);
 const itemsPerPage = 4;
 
-let projects = ref([]);
+let proje = ref([]);
 let totalProjects = ref(0);
 let pageCount = ref(0);
 
-const fetchProjects = async () => {
-  const { pending, data } = useLazyFetch("https://api.github.com/users/cm-ca/repos", {
-    method: 'GET',
-    lazy: true,
-  });
 
-  if (!pending.value) {
-    projects.value = data.value;
-    totalProjects.value = projects.value.length;
-    pageCount.value = Math.ceil(totalProjects.value / itemsPerPage);
-  }
-};
+const { data, data: projects } = useLazyFetch("https://api.github.com/users/cm-ca/repos", {
+  method: 'GET',
 
-onMounted(fetchProjects);
+});
+
+
+projects.value = data.value;
+totalProjects.value = proje.value.length;
+pageCount.value = Math.ceil(totalProjects.value / itemsPerPage);
 
 const paginatedProjects = computed(() => {
   const start = (currentPage.value - 1) * itemsPerPage;
   const end = start + itemsPerPage;
-  return projects.value.slice(start, end);
+  return proje.value.slice(start, end);
 });
 </script>
