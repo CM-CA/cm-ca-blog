0db576ea11dccc0bc01cb4881c691872eae6255d
Camilo Manuel Couselo Alonso <manuel.couseloalonso@gmail.com>
2024-04-25 23:07:31 +0200

    Update [slug].vue

--- pages/articles/[slug].vue
+++ pages/articles/[slug].vue
@@ -14,10 +14,8 @@
     <div class="flex items-center justify-center p-2.5 gap-2">
    <SocialShare networks="linkedin" 
    :network="'linkedin'"
-   :styled="true"
-   :label="false" />
+    />
   </div>
-
   </main>
 </template>
 
