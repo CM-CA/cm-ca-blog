3f6c855a011e0d0a1832a59418b851738be82f62
cm-ca <manuel.couseloalonso@gmail.com>
2024-04-25 17:50:11 +0200

    Update

--- pages/articles/[slug].vue
+++ pages/articles/[slug].vue
@@ -18,11 +18,11 @@ const route = useRoute();
 const { slug } = route.params;
 useSeoMeta({
   title: 'Okud4',
-  ogTitle: 'Okud4',
+  ogTitle: `Okud4 - ${slug}`,
   description: 'Mi blog personal',
   ogDescription: 'Mi blog personal',
   ogUrl: `https://cm-ca.vercel.app/articles/${slug}`,
-  ogImage: 'https://cm-ca.vercel.app/assets/favicons/mstile-150x150.png',
+  ogImage: 'https://cm-ca.vercel.app/assets/avatar.jpg',
   icon: 'https://cm-ca.vercel.app/assets/favicons/mstile-150x150.png',
   twitterCard: 'summary_large_image',
   lang: 'es',
