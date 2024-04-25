4d9a34f6fcb4f2173b49efb31d0b0d1c7f76dfc4
cm-ca <manuel.couseloalonso@gmail.com>
2024-04-25 18:56:13 +0200

    Update [slug].vue

--- pages/articles/[slug].vue
+++ pages/articles/[slug].vue
@@ -4,25 +4,37 @@
       class="prose dark:prose-invert prose-blockquote:not-italic prose-pre:bg-gray-900 prose-img:ring-1 prose-img:ring-gray-200 dark:prose-img:ring-white/10 prose-img:rounded-lg">
       <ContentDoc v-slot="{ doc }" tag="article">
         <article>
-          <NuxtImg src="/img/banner.jpeg"/>
+          <NuxtImg src="/img/banner.jpeg" />
           <h1>{{ doc.title }}</h1>
           <ContentRenderer :value="doc" />
         </article>
       </ContentDoc>
     </div>
-    <AppShareButton/>
+    <AppShareButton />
   </main>
 </template>
 
 <script setup>
 const route = useRoute();
 const { slug } = route.params;
+useHead({
+  htmlAttrs: {
+    lang: 'es'
+  },
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      href: '/favicon.png'
+    }
+  ]
+})
 useSeoMeta({
 
   ogTitle: `Okud4 - ${slug}`,
-   ogImage: '/img/banner.jpeg',
-   ogDescription: 'Post de Okud4',
-   ogUrl: `https://cm-ca.vercel.app/articles/${slug}`,
+  ogImage: '/img/banner.jpeg',
+  ogDescription: 'Post de Okud4',
+  ogUrl: `https://cm-ca.vercel.app/articles/${slug}`,
 
 });
 </script>
