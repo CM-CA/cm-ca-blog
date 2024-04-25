bc1157c63c2b7204c765a80defc7960c2907c367
Camilo Manuel Couselo Alonso <manuel.couseloalonso@gmail.com>
2024-04-25 23:09:16 +0200

    Update [slug].vue

--- pages/articles/[slug].vue
+++ pages/articles/[slug].vue
@@ -1,7 +1,8 @@
 <template>
   <main class="min-h-screen">
     <div
-      class="prose dark:prose-invert prose-blockquote:not-italic prose-pre:bg-gray-900 prose-img:ring-1 prose-img:ring-gray-200 dark:prose-img:ring-white/10 prose-img:rounded-lg">
+      class="prose dark:prose-invert prose-blockquote:not-italic prose-pre:bg-gray-900 prose-img:ring-1 prose-img:ring-gray-200 dark:prose-img:ring-white/10 prose-img:rounded-lg"
+    >
       <ContentDoc v-slot="{ doc }" tag="article">
         <article>
           <NuxtImg src="/img/banner.jpeg" />
@@ -10,12 +11,10 @@
         </article>
       </ContentDoc>
     </div>
-      
+
     <div class="flex items-center justify-center p-2.5 gap-2">
-   <SocialShare networks="linkedin" 
-   :network="'linkedin'"
-    />
-  </div>
+      <SocialShare networks="linkedin" :network="'linkedin'" :styled="true" :label="false" />
+    </div>
   </main>
 </template>
 
@@ -24,23 +23,21 @@ const route = useRoute();
 const { slug } = route.params;
 useHead({
   htmlAttrs: {
-    lang: 'es'
+    lang: "es",
   },
   link: [
     {
-      rel: 'icon',
-      type: 'image/png',
-      href: '/favicon.png'
-    }
-  ]
-})
+      rel: "icon",
+      type: "image/png",
+      href: "/favicon.png",
+    },
+  ],
+});
 useSeoMeta({
-
   ogTitle: `Okud4 - ${slug}`,
-  ogImage: '/img/banner.jpeg',
-  ogDescription: 'Post de Okud4',
+  ogImage: "/img/banner.jpeg",
+  ogDescription: "Post de Okud4",
   ogUrl: `https://cm-ca.vercel.app/articles/${slug}`,
-
 });
 </script>
 <style>
