25df0ea350aff16ed0481fee8c4011c0c9414115
cm-ca <manuel.couseloalonso@gmail.com>
2024-04-25 13:10:41 +0200

    Update [slug].vue

--- pages/articles/[slug].vue
+++ pages/articles/[slug].vue
@@ -8,8 +8,20 @@
           <h1>{{ doc.title }}</h1>
           <ContentRenderer :value="doc" />
         </article>
-      </ContentDoc>
+      </ContentDoc> 
+      <div class="fixed right-4 bottom-4">
+      <a
+        href="https://www.linkedin.com/sharing/share-offsite/?url=URL"
+        target="_blank"
+        class="py-2 px-4 rounded-lg bg-white/80 dark:bg-gray-800/80 dark:text-white"
+      >
+        <Icon name="mdi:linkedin" class="inline-block" />
+        <span class="ml-2">Compartir en LinkedIn</span>
+      </a>
     </div>
+
+    </div>
+   
   </main>
 </template>
 
